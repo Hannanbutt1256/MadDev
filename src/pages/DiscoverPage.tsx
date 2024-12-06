da236a4c2c26353ce4d72c9703e15c636a154d02
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-07 02:17:17 +0500

    Can be seen on hompage but are called in Tab.tsx file

---
+++ src/pages/DiscoverPage.tsx
@@ -0,0 +1,5 @@
+const DiscoverPage = () => {
+  return <div className="h-full m-2 p-2">DiscoverPage</div>;
+};
+
+export default DiscoverPage;
---
+++ src/pages/FollowedPage.tsx
@@ -0,0 +1,5 @@
+const FollowedPage = () => {
+  return <div className="h-full m-2 p-2">FollowedPage</div>;
+};
+
+export default FollowedPage;
