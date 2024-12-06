56ba98afe8a0299d5d5930aed55c50f077d560d0
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-06 11:41:21 +0500

    changed comment.ts

--- src/types/comment.ts
+++ src/types/comment.ts
@@ -1,6 +1,6 @@
 export interface BlogCommentInterface {
   id: string; // Unique comment ID
-  postId: string;
+  postId: string; // Uniquie postID of post for which the comment is made
   authorId: string;
   content: string;
   createdAt: Date; // Comment creation timestamp
