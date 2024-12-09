e32c48205c23cd8cb4ff7eb1a061085b2902832f
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-09 13:08:38 +0500

    utilized card component in it

--- src/pages/DiscoverPage.tsx
+++ src/pages/DiscoverPage.tsx
@@ -1,5 +1,48 @@
+import BlogCard from "../components/BlogCard";
+
 const DiscoverPage = () => {
-  return <div className="h-full m-2 p-2">DiscoverPage</div>;
+  return (
+    <div className="min-h-screen flex flex-col items-center md:block   ">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  2xl:grid-cols-4 gap-4">
+        <BlogCard
+          key={"1"}
+          title={"React 18 Features You Should Know"}
+          description={"An overview of the exciting features in React 18."}
+          author={"John Doe"}
+          createdAt={"2024-12-01T12:00:00Z"}
+          tags={["React", "JavaScript", "Web Development"]}
+          coverImage={"https://picsum.photos/200/300"}
+        />
+        <BlogCard
+          key={"2"}
+          title={"React 18 Features You Should Know"}
+          description={"An overview of the exciting features in React 18."}
+          author={"John Doe"}
+          createdAt={"2024-12-01T12:00:00Z"}
+          tags={["React", "JavaScript", "Web Development"]}
+          coverImage={"https://picsum.photos/200/300"}
+        />
+        <BlogCard
+          key={"3"}
+          title={"React 18 Features You Should Know"}
+          description={"An overview of the exciting features in React 18."}
+          author={"John Doe"}
+          createdAt={"2024-12-01T12:00:00Z"}
+          tags={["React", "JavaScript", "Web Development"]}
+          coverImage={"https://picsum.photos/200/300"}
+        />
+        <BlogCard
+          key={"4"}
+          title={"React 18 Features You Should Know"}
+          description={"An overview of the exciting features in React 18."}
+          author={"John Doe"}
+          createdAt={"2024-12-01T12:00:00Z"}
+          tags={["React", "JavaScript", "Web Development"]}
+          coverImage={"https://picsum.photos/200/300"}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default DiscoverPage;
