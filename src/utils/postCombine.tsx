2acaf17b63660aa4c460361d444eefe5218e5b78
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-19 11:02:37 +0500

    use it to merge data from two localstorage objects in one object

---
+++ src/utils/postCombine.tsx
@@ -0,0 +1,35 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import { BlogPostInterface } from "../types/post";
+
+export const useGetPostDataFromLocalStorage = (): BlogPostInterface | null => {
+  const [user] = useAuthState(auth);
+  const editorContent = localStorage.getItem("editorContent");
+  const editPageDataString = localStorage.getItem("editPageData");
+
+  if (!editorContent || !editPageDataString) {
+    console.error("Missing data in localStorage.");
+    return null;
+  }
+
+  try {
+    const editPageData = JSON.parse(editPageDataString);
+    const newPost: BlogPostInterface = {
+      id: "",
+      title: editPageData.title,
+      authorId: user?.uid || "",
+      content: editorContent,
+      tags: editPageData.tags,
+      coverImage: editPageData.coverImage || "",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      likes: 0,
+      comments: [],
+    };
+
+    return newPost;
+  } catch (error) {
+    console.error("Error parsing localStorage data:", error);
+    return null;
+  }
+};
