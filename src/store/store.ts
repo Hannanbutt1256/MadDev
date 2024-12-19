f688890e6037cb68ee2ad3877d6441e4865624f1
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-19 10:58:44 +0500

    commit

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userProfileReducer from "./user/userProfileSlice";
-
+import blogPostReducer from "./posts/blogPostSlice";
+import blogSliceReducer from "./posts/allBlogsData";
+import podcastSliceReducer from "./podcast/podcastSlice";
 export const store = configureStore({
   reducer: {
     userProfile: userProfileReducer,
+    blogPosts: blogPostReducer,
+    allBlogData: blogSliceReducer,
+    podcastData: podcastSliceReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
