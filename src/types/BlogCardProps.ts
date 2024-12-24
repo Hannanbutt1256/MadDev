9bda7b262de7174864a0058a1ca166b9ba1de68f
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-25 01:36:32 +0500

    feat(BlogCardProps): allow author to be a ReactNode for enhanced flexibility

--- src/types/BlogCardProps.ts
+++ src/types/BlogCardProps.ts
@@ -1,6 +1,8 @@
+import React from "react";
+
 export interface BlogCardProps {
   title: string;
-  author: string;
+  author: string | React.ReactNode;
   createdAt: string; // ISO string
   tags?: string[];
   coverImage?: string; // URL of the blog's cover image
