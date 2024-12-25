22b45b265eb3a9b8342a23b91b132a8265ef4131
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-25 21:55:10 +0500

    feat(blogSlice): enhance blog state management with pagination and loading indicators

--- src/store/posts/allBlogsData.ts
+++ src/store/posts/allBlogsData.ts
@@ -1,12 +1,16 @@
+// blogSlice.ts
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { fetchAllBlogPosts } from "./postThunks"; // Adjust path if necessary
+import { fetchAllBlogPosts } from "./postThunks";
 import { BlogPostInterface } from "../../types/post";
+import { DocumentSnapshot } from "firebase/firestore";
 
 interface BlogState {
   posts: BlogPostInterface[];
-  status: string; // loading, success, or error
+  status: string;
   error: string | null;
   hasMore: boolean;
+  lastVisible: DocumentSnapshot | null;
+  isLoadingMore: boolean;
 }
 
 const initialState: BlogState = {
@@ -14,6 +18,8 @@ const initialState: BlogState = {
   status: "idle",
   error: null,
   hasMore: true,
+  lastVisible: null,
+  isLoadingMore: false,
 };
 
 const blogSlice = createSlice({
@@ -22,24 +28,40 @@ const blogSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchAllBlogPosts.pending, (state) => {
-        state.status = "loading";
+      .addCase(fetchAllBlogPosts.pending, (state, action) => {
+        if (!action.meta.arg?.lastVisible) {
+          state.status = "loading";
+        } else {
+          state.isLoadingMore = true;
+        }
       })
       .addCase(
         fetchAllBlogPosts.fulfilled,
-        (state, action: PayloadAction<BlogPostInterface[]>) => {
+        (
+          state,
+          action: PayloadAction<{
+            posts: BlogPostInterface[];
+            lastVisible: DocumentSnapshot | null;
+          }>
+        ) => {
           state.status = "success";
-          const newPosts = action.payload.filter(
+          state.isLoadingMore = false;
+
+          const newPosts = action.payload.posts.filter(
             (newPost) =>
               !state.posts.some(
                 (existingPost) => existingPost.id === newPost.id
               )
           );
+
           state.posts = [...state.posts, ...newPosts];
+          state.lastVisible = action.payload.lastVisible;
+          state.hasMore = action.payload.posts.length === 10;
         }
       )
       .addCase(fetchAllBlogPosts.rejected, (state, action) => {
         state.status = "error";
+        state.isLoadingMore = false;
         state.error = action.error.message || "Failed to fetch blog posts";
       });
   },
