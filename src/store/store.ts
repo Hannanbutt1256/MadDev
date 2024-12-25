e001507261ae95152b1579d5f227f7a8e44603d4
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-25 21:55:00 +0500

    feat(store): add notification reducer to the store

--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,15 @@ import userProfileReducer from "./user/userProfileSlice";
 import blogPostReducer from "./posts/blogPostSlice";
 import blogSliceReducer from "./posts/allBlogsData";
 import podcastSliceReducer from "./podcast/podcastSlice";
+import notificationReducer from "./notification/notificationSlice";
+
 export const store = configureStore({
   reducer: {
     userProfile: userProfileReducer,
     blogPosts: blogPostReducer,
     allBlogData: blogSliceReducer,
     podcastData: podcastSliceReducer,
+    notifications: notificationReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
