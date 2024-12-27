69c0b488cbbabb3e718b78d8c79667b7383b3888
Hannan Butt <hannanbutt1256@gmail.com>
2024-12-27 14:40:11 +0500

    feat(toast): add themed toast container with light and dark styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,35 @@
+/* eslint-disable react-refresh/only-export-components */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
 import routes from "./routes/routes.tsx";
 import "./index.css";
-import { ThemeProvider } from "./common/ThemeProvider.tsx";
+import { ThemeProvider, useTheme } from "./common/ThemeProvider.tsx";
 import { Provider } from "react-redux";
 import { store } from "./store/store.ts";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const router = createBrowserRouter(routes);
+
+const ThemedToastContainer = () => {
+  const { isDarkMode } = useTheme();
+  return (
+    <ToastContainer
+      className={isDarkMode ? "dark" : ""}
+      theme={isDarkMode ? "dark" : "light"}
+      position="top-right"
+      style={{ top: "75px" }}
+    />
+  );
+};
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <ThemeProvider>
         <RouterProvider router={router} />
+        <ThemedToastContainer />
       </ThemeProvider>
     </Provider>
   </StrictMode>
